b60ec5205bbda97022126a03c0e064c97d321109
jaimeenahn <jahn5@buffalo.edu>
2019-03-24 09:30:56 +0900

    doc is dropped from vocab

--- build_graph.py
+++ build_graph.py
@@ -21,7 +21,8 @@ train_data = read_data('data/ratings_train.txt')
 print("===========================================")
 print("Load Data Complete!")
 print("===========================================")
-#train_data = train_data[:10] #samples
+
+train_data = train_data[:10000] #samples
 #tokenize
 okt = Okt()
 reviews = []
@@ -45,35 +46,76 @@ reviews = list of sentences
 tokenized_reviews = [tokenized_review]
 
 """
+
 print("===========================================")
 print("Data Tokenize Complete!")
 print("===========================================")
 
+data = {}
+data['train_data'] = train_data
+
+f = open("data/token.pkl", 'wb')
+pickle.dump(data, f)
+f.close()
+
+train_data = data['train_data']
+
 #build vocab
 vocab = list()
+word_dict = {}
 num_doc = 0
 
 #add documents in vocab
+"""
 for review in train_data :
     if review[0] not in vocab :
         vocab.append(review[0])
-num_doc = len(vocab)
+"""
+num_doc = len(train_data)
 #add words in vocab
 for review in train_data :
     for word in review[1] :
         if word not in vocab :
             vocab.append(word)
+        if word not in word_dict :
+            word_dict[word] = 1
+        else :
+            word_dict[word] = word_dict[word] + 1
 
 print("===========================================")
 print("Build Vocabulary Complete!")
 print("Number of documents : ", num_doc)
-print("Number of words : ", len(vocab) - num_doc)
+print("Number of words : ", len(vocab))
 print("Vocab Size : ", len(vocab))
 print("===========================================")
-#build A
-A = np.zeros((len(vocab), len(vocab)))
 
-tfidf = TfidfVectorizer(max_features=len(vocab)-num_doc)
+
+data['vocab'] = vocab
+data['word_dict'] = word_dict
+
+f = open("data/vocab.pkl", 'wb')
+pickle.dump(data, f)
+f.close()
+
+new_vocab = []
+for i in range(len(vocab)) :
+    if i < num_doc :
+        new_vocab.append(vocab[i])
+    else :
+        if(word_dict[vocab[i]] > 1) :
+            new_vocab.append(vocab[i])
+
+print("Size of New Vocab : ", len(new_vocab))
+data['new_vocab'] = new_vocab
+
+f = open("data/new_vocab.pkl", 'wb')
+pickle.dump(data, f)
+f.close()
+
+#build A
+A = np.zeros((len(vocab)+num_doc, len(vocab)+num_doc))
+
+tfidf = TfidfVectorizer()
 X = tfidf.fit_transform(reviews)
 X = X.toarray()
 Y = tfidf.get_feature_names()
@@ -81,18 +123,22 @@ Y = tfidf.get_feature_names()
 windows = make_window(tokenized_reviews, 20)
 num_windows = len(windows)
 
-word_freq = count_word_freq(vocab[num_doc:], windows)
-pair_freq = count_pair_freq(vocab[num_doc:], windows)
+word_freq = count_word_freq(vocab, windows)
+print("===========================================")
+print("Counting Word Frequency Complete!")
+print("===========================================")
+pair_freq = count_pair_freq(vocab, windows)
+print("===========================================")
+print("Counting pair Frequency Complete!")
+print("===========================================")
 
-for i in range(len(vocab)) :
-    for j in range(i, len(vocab)) :
+
+
+for i in range(len(vocab)+num_doc) :
+    for j in range(i, len(vocab)+num_doc) :
         if i == j :
             A[i][j] = 1
-        elif i < num_doc and j >= num_doc and vocab[j] in tfidf.vocabulary_ :
-            # i is document and j is word A[i][j] = TF-IDF
-            idx = tfidf.vocabulary_.get(vocab[j])
-            A[i][j] = X[i][idx]
-        elif i >=num_doc and j>=num_doc :
+        elif i < len(vocab) and j < len(vocab) :
             # i , j is word A[i][j] is PMI
             pi = 1.0 * (word_freq[vocab[i]] / num_windows)
             pj = 1.0 * (word_freq[vocab[j]] / num_windows)
@@ -101,7 +147,11 @@ for i in range(len(vocab)) :
                 pmi = log(pij/(pi*pj))
                 if pmi >= 0 :
                     A[i][j] = pmi
-    if i%100==0 :
+        elif i >= len(vocab) and j < len(vocab) and vocab[j] in tfidf.vocabulary_ :
+            # i is document and j is word A[i][j] = TF-IDF
+            idx = tfidf.vocabulary_.get(vocab[j])
+            A[i][j] = X[i-num_doc][idx]
+    if i%20==0 :
         print(i, "th row finished")
 
 print("===========================================")
@@ -109,7 +159,7 @@ print("Build A Complete!")
 print("A shape : ", A.shape)
 print("===========================================")
 
-X = np.eye(len(vocab))
+X = np.eye(len(vocab)+num_doc)
 
 data = {}
 data['train_data'] = train_data
